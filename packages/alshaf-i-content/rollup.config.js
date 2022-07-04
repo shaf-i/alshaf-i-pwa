95e05cac887e1c42add7a3479d8bacaa3d86881c
Amir Najafi <njfamirm@gmail.com>
2022-07-04 12:37:55 +0430

    chore(content): change nginx config

--- packages/alshaf-i-content/rollup.config.js
+++ packages/alshaf-i-content/rollup.config.js
@@ -76,7 +76,7 @@ export default {
     summary({showMinifiedSize: false}), // Print bundle summary
 
     copy({
-      patterns: ['images/**/*'],
+      patterns: ['images/**/*', 'robots.txt'],
     }),
   ],
 
