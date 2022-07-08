4918d0670693d52cb554fa60c83099724465c4a7
Amir Najafi <njfamirm@gmail.com>
2022-07-09 01:31:59 +0430

    feat(api-server): load env file for token

--- packages/alshaf-i-api-server/src/config.ts
+++ packages/alshaf-i-api-server/src/config.ts
@@ -4,12 +4,19 @@ import {createLogger} from './@alwatr/logger/src/logger.js';
 
 export const logger = createLogger('api-server');
 
+// load config from .env file
 if (process.env.DEBUG_MODE == 'production') {
-  // load .env file
   config();
 }
 export const port: number = parseInt(process.env.PORT || '') || 8080;
 export const allowOriginHost: string = process.env.ALLOW_ORIGIN_HOST || '*';
-logger.logMethodArgs('load_conifg', {port: port, allow_origin_host: allowOriginHost});
+
+// load token from .env file
+config({path: '../../.env'})
+export const apiToken: string | undefined = process.env.API_TOKEN;
+if (apiToken === undefined) {
+  logger.accident('load_token', 'undefined_token', 'Cannot load token from .env file')
+  process.exit(1)
+}
 
 export const jsonFilePath = 'data/user.json';
